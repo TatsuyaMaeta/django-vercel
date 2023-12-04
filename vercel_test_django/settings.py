378a052476b5d43b67cadf68b3a798df8e7c7e23
TatsuyaMaeta <tat.mae084@gmail.com>
2023-12-04 15:57:48 +0900

    fix

--- vercel_test_django/settings.py
+++ vercel_test_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2bgke*n1@2c+vql1a^ia%##zwy=dv12_^y6x_zqv3@*v$s#co$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app"]
 
